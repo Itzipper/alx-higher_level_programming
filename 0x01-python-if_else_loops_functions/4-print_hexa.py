ffb04e4f2429f0df08a3f91ce2ac2f140842e5dd
Itzipper <ajisafelukmanm@gmail.com>
2022-11-30 10:51:29 +0000

    Task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    if (character != 101 and character != 113):
-        print("{:c}".format(character), end='')
+for num in range(0, 99):
+    print("{:d} = 0x{:x}".format(num, num))
