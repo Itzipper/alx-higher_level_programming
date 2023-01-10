941c18decfcf08ca6106636dd4aa8b9b0faab87f
Itzipper <ajisafelukmanm@gmail.com>
2023-01-10 23:13:54 +0000

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,5 @@
+e File  29 lines (25 sloc)  963 Bytes
 #!/usr/bin/python3
-# 12-student.py
-# Ajisafe Lukman
 """Defines a class Student."""
 
 
@@ -9,9 +8,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-
         Args:
-
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
@@ -22,14 +19,12 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-
         If attrs is a list of strings, represents only those attributes
         included in the list.
         Args:
-
             attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        return self.
